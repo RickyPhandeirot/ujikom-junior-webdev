9a39eabf5f3e8b901f5dba190b9c43a443b26c9b
Ricky Phandeirot <rickyphandeirot@gmail.com>
2024-06-21 23:01:34 +0800

    mengedit landing, register dan login page

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -29,16 +29,48 @@
     <!-- Template Main CSS File -->
     <link href="{{ asset('') }}assets/css/style.css" rel="stylesheet">
 
-    <!-- =======================================================
-  * Template Name: Scaffold
-  * Template URL: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/
-  * Updated: Mar 17 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+    <style>
+        body,
+        html {
+            height: 100%;
+            margin: 0;
+        }
+
+        .full-page-bg {
+            background-image: url('assets/img/bground.jpg');
+            /* Ganti dengan path gambar background Anda */
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 100%;
+            width: 100%;
+        }
+
+        .login-box {
+            background-color: rgba(255, 255, 255, 0.8);
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            text-align: center;
+            width: 25%;
+            /* Ubah lebarnya sesuai kebutuhan */
+            max-width: 400px;
+            /* Lebar maksimum jika menggunakan persentase */
+            margin: 0 auto;
+            /* Agar kotak berada di tengah halaman */
+        }
+
+        .login-box h1 {
+            margin-bottom: 20px;
+        }
+
+        .login-box .btn {
+            margin-top: 10px;
+        }
+    </style>
 </head>
 
-<body>
+<body class="full-page-bg">
 
     <!-- ======= Header ======= -->
     <header id="header" class="fixed-top d-flex align-items-center">
@@ -53,10 +85,23 @@
             <nav id="navbar" class="navbar order-last order-lg-0">
                 <ul>
                     <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-                    <li><a class="nav-link scrollto" href="#services">Services</a></li>
-                    <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-                    <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
-                    <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                    @if (Auth::check())
+                        {{-- jika sudah tampilan menu dashboard dan logout --}}
+                        <li class="nav-item">
+                            <a class="nav-link page-scroll" href="{{ route('dashboard') }}">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link page-scroll" href="{{ route('logout') }}">Logout</a>
+                        </li>
+                    @else
+                        {{-- jika belum tampilkan register dan login --}}
+                        <li class="nav-item">
+                            <a class="nav-link page-scroll" href="{{ route('register') }}">Register</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link page-scroll" href="{{ route('login') }}">Login</a>
+                        </li>
+                    @endif
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
             </nav><!-- .navbar -->
@@ -71,8 +116,8 @@
             <div class="row">
                 <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center"
                     data-aos="fade-up">
-                    <div>
-                        <h1>Selamat Datang Di Pencatatan Pegawai</h1>
+                    <h1>Selamat Datang Di Pencatatan Pegawai</h1>
+                    <div class="login-box">
                         @if (Auth::check())
                             <a class="btn-solid-lg page-scroll" href="{{ route('dashboard') }}">Dashboard</a>
                         @else
@@ -81,7 +126,7 @@
                     </div>
                 </div>
                 <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left">
-                    <img src="assets/img/hero-img.png" class="img-fluid" alt="">
+                    <img src="assets/img/pegawaiimg2.png" class="img-fluid" alt="">
                 </div>
             </div>
         </div>
@@ -95,79 +140,7 @@
 
   </main><!-- End #main --> --}}
 
-    <!-- ======= Footer ======= -->
-    {{-- <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
 
-          <div class="col-lg-3 col-md-6">
-            <div class="footer-info">
-              <h3>Scaffold</h3>
-              <p>
-                A108 Adam Street <br>
-                NY 535022, USA<br><br>
-                <strong>Phone:</strong> +1 5589 55488 55<br>
-                <strong>Email:</strong> info@example.com<br>
-              </p>
-              <div class="social-links mt-3">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Services</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Scaffold</span></strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/ -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </div>
-  </footer><!-- End Footer --> --}}
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
